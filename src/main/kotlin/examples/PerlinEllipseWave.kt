f3ad4570528077632c63bad7842e263aae2b35eb
Piyush Pradeepkumar <piyushvp1@gmail.com>
2021-10-15 23:38:36 +0530

    Added first example using Perlin noise

--- src/main/kotlin/examples/PerlinEllipseWave.kt
+++ src/main/kotlin/examples/PerlinEllipseWave.kt
@@ -1,4 +1,48 @@
 package examples
 
-class PerlinEllipseWave {
+import processing.core.PApplet
+
+class PerlinEllipseWave : PApplet() {
+
+    init {
+        setSize(800, 600)
+        runSketch()
+    }
+
+
+    override fun setup() {
+        background(20)
+        smooth(4)
+    }
+
+    private var startOffset = 0f
+
+    override fun draw() {
+
+        background(20)
+
+        stroke(255)
+
+        noFill()
+
+        var xOffset = startOffset
+
+        beginShape()
+
+        (0..width).forEach {
+            if (it % 24 == 0) {
+                val radius = map(noise(it.toFloat()), 0f, 1f, 0f, 24f)
+                val offsetY = noise(xOffset) * height
+                ellipse(it.toFloat(), offsetY, radius, radius)
+                vertex(it.toFloat(), offsetY)
+            }
+            xOffset += 0.01f
+        }
+
+        endShape()
+
+//        noLoop()
+
+        startOffset += 0.001f
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,45 +1,5 @@
-import processing.core.PApplet
-
-class ProcessingWrapper : PApplet() {
-
-    init {
-        setSize(600, 600)
-        runSketch()
-    }
-
-
-    override fun setup() {}
-
-    private var timeOffset = 0f
-
-    var xOffset = 0f
-
-    override fun draw() {
-        timeOffset += 0.001f
-
-        background(20)
-
-        stroke(255)
-
-        noFill()
-
-//        beginShape()
-
-
-        (0..width).forEach {
-            if (it % 30 == 0) {
-                val y = noise(xOffset) * height
-                ellipse(it.toFloat(), y, 4f, 4f)
-            }
-            xOffset += 0.001f
-        }
-
-//        endShape()
-
-//        noLoop()
-    }
-}
+import examples.PerlinEllipseWave
 
 fun main(args: Array<String>) {
-    ProcessingWrapper()
+    PerlinEllipseWave()
 }
